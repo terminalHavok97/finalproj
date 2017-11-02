1ef52cc92a0494d4f85e352daf969c23757fb299
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-02 15:44:41 +0000

    Testing TTS

--- main.py
+++ main.py
@@ -7,3 +7,15 @@
 #play audio of sentance 1, then sentance 2
 #buttons to choose which is better
 #update elo rank
+
+#Text to speech test
+
+from gtts import gTTS
+import os
+import vlc
+
+tts = gTTS(text='Hello fellow human how are you?', lang='en')
+tts.save("good.mp3")
+
+p = vlc.MediaPlayer("good.mp3")
+p.play()
