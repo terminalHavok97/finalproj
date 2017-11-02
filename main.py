c38ed83efb9cbe4ab107712ff11cf35990f9e438
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-02 15:17:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Tom Vaughan - tv15461
+
+#prelim experiment
+#import word lists
+#build them so they're grammatically good bois
+#blank white/black screen
+#play audio of sentance 1, then sentance 2
+#buttons to choose which is better
+#update elo rank
