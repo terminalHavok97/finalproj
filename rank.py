6aad7c8d3853d587293fb7304f5eea674d65af27
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-26 22:20:21 +0000

    Added new comparisons field into table rows, tells us how many matchups each sentance has been in

--- rank.py
+++ rank.py
@@ -1,13 +1,13 @@
 class Ranker:
     #Make the table, define the structure of a row
-    #Row = id, rank, data(not sure if string or array)
+    #Row = id, rank, comparisons, data(not sure if string or array)
     def __init__(self):
         self.table = []
         self.t_length = 0
 
     #Add a new sentance to the table
     def addToTable(self, data):
-        l = [self.t_length, 1000, data]
+        l = [self.t_length, 1000, 0, data]
         self.table.append(l)
         self.t_length += 1
 
@@ -26,7 +26,7 @@ class Ranker:
     #Print everything in the table
     def printAll(self):
         for i in self.table:
-            print i[0], i[1], i[2]
+            print i[0], i[1], i[2], i[3]
 
         print ""
         print "===================="
@@ -36,13 +36,13 @@ class Ranker:
     #Search table for data matching param data and return its id
     def getID(self, data):
         for i in self.table:
-            if i[2] == data:
+            if i[3] == data:
                 return i[0]
         print "Error - Data not found"
         return -1
     #Search table for id matching param id and return its data
     def getData(self, ID):
-        return self.table[ID][2]
+        return self.table[ID][3]
 
     #Search table for id matching param id and return its rank
     def getRank(self, ID):
@@ -52,6 +52,9 @@ class Ranker:
     #1 point for win, 0 for loss
     def updateFromComparison(self, ID1, ID2):
         k = 20
+
+        self.table[ID1][2] += 1
+        self.table[ID2][2] += 1
 
         qa = pow(10, self.getRank(ID1) / float(400))
         qb = pow(10, self.getRank(ID2) / float(400))
@@ -67,10 +70,7 @@ class Ranker:
         print "SUM: ", (ea1 + ea2)
 
         rank1 = self.getRank(ID1) + (k * (1 - ea1))
-        rank2 = self.getRank(ID2) + (k * (-1 - ea2))
-
-        #print "rank1 = ", rank1
-        #print "rank2 = ", rank2
+        rank2 = self.getRank(ID2) + (k * (0 - ea2))
 
         self.table[ID1][1] = rank1
         self.table[ID2][1] = rank2
