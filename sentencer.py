1c9539959221b821a59b803c824ce53d3d233003
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-26 12:38:32 +0000

    Worked out how to do private methods

--- sentencer.py
+++ sentencer.py
@@ -7,25 +7,25 @@ class Sentencer:
 
     #Read in word lists
     def __init__(self):
-        self.nouns      = self.readWords("assets/nouns.txt")
-        self.verbs      = self.readWords("assets/verbs.txt")
-        self.adjs       = self.readWords("assets/adjectives.txt")
+        self.nouns      = self.__readWords("assets/nouns.txt")
+        self.verbs      = self.__readWords("assets/verbs.txt")
+        self.adjs       = self.__readWords("assets/adjectives.txt")
 
     #Give the user a sentance
     #Assuming SUBJECT VERB ADJ NOUN structure
     def getSentence(self):
-        verb = self.verbs[self.randomWord(self.verbs)]
-        adj  = self.adjs[self.randomWord(self.adjs)]
+        verb = self.verbs[self.__randomWord(self.verbs)]
+        adj  = self.adjs[self.__randomWord(self.adjs)]
         while (True):
-            noun1 = self.nouns[self.randomWord(self.nouns)]
-            noun2 = self.nouns[self.randomWord(self.nouns)]
+            noun1 = self.nouns[self.__randomWord(self.nouns)]
+            noun2 = self.nouns[self.__randomWord(self.nouns)]
             if noun1 != noun2:
                 break
         result = [noun1, verb, adj, noun2]
         return result
 
     #Read in a specific word list
-    def readWords(self, fname):
+    def __readWords(self, fname):
         with open(fname) as f:
             words = f.readlines()
         words = [x.strip() for x in words]
@@ -33,5 +33,5 @@ class Sentencer:
         return words
 
     #Get random index in a list
-    def randomWord(self, list):
+    def __randomWord(self, list):
         return random.randrange(0, (len(list) - 1), 1)
