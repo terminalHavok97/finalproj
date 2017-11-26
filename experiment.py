429456ef15996674ad229d7b3a2162dc547e9e4c
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-26 23:53:46 +0000

    Starting building Experiment

--- experiment.py
+++ experiment.py
@@ -10,7 +10,18 @@ class Experiment
     global threading, Sentencer, Ranker, AudioPlayer, time
 
     #Make table of sentances, initialise everything
-    def __init__(self):
+    def __init__(self, number):
+        #Inits
+        self.fname = "bin/tta.mp3"
+        self.sGen = Sentencer()
+        self.ranker = Ranker()
+        self.ap = AudioPlayer()
+
+        #Gen arg number of sentences
+        for i in range(0, number):
+            sen = self.sGen.getSentenceString()
+            self.ranker.addToTable(sen)
+
         self.start()
 
     #Start experiment, play audio of each pair of sentances and choose which is
@@ -20,4 +31,4 @@ class Experiment
     #Stop experiment
     def stop():
 
-    def display():
+    def __display():
--- main.py
+++ main.py
@@ -9,21 +9,4 @@ import threading as t
 import sys
 import os
 
-fname = "bin/tta.mp3"
-sGen = Sentencer()
-ranker = Ranker()
-
-for i in range(0,3):
-    s = sGen.getSentenceString()
-    ranker.addToTable(s)
-
-#0 wins against 2
-ranker.printAll()
-ranker.updateFromComparison(0, 1)
-ranker.printAll()
-ranker.updateFromComparison(0, 2)
-ranker.printAll()
-ranker.updateFromComparison(1, 0)
-ranker.printAll()
-ranker.updateFromComparison(0, 2)
-ranker.printAll()
+exp = Experiment(50)
