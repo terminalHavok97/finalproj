c0885d369dd073263e7fabfaad395c15ee1f0e4c
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-11-26 17:43:14 +0000

    Testing to see if words will generate then play, and added .pyo to ignore

--- main.py
+++ main.py
@@ -7,14 +7,10 @@ import sys
 import os
 
 fname = "bin/tta.mp3"
-
-#ap = AudioPlayer()
-#ap.textToAudio("Oi Oi Savaloy", fname)
-#ap.playSavedAudio(fname)
-
-
 sGen = Sentencer()
+ap = AudioPlayer()
 
 for i in range(1,10):
     result = sGen.getSentenceString()
-    print result
+    ap.textToAudio(result, fname)
+    ap.playSavedAudio(fname)
