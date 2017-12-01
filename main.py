fcdb70ff6adb9547ba80d41fec50f3b5d31f600c
Tom Vaughan <tv15461@my.bristol.ac.uk>
2017-12-01 12:00:45 +0000

    Met with Conor, added notes for what to do next as comments in main.py

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ except ImportError:
 
 exp = Experiment(3, 5, 10)
 
-#TODO Get cmd arguments in????
 #TODO Work out how sample sizes and multiple participants will work
+#TODO Look at pruning out some bad sentences
+#TODO Attention trap - Every n sentances, do something weird requiring a 3rd type
+# of key press. Could do a fish sentance?
+#TODO Look into web page serving - probably Javascript of psypy
+#TODO Simple message passing between python serving and JS client
+#TODO Add rank element based on a gaussian, so matching isn't just based
+# on least played
+#TODO Make GUI class for word matching - only eeg needs to be audio only
