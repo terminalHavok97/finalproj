6af02ddab87accf3485c1b7e591a9c9133333809
Tom Vaughan <tv15461@my.bristol.ac.uk>
2018-02-03 17:57:40 +0000

    Sentances now sent to client

--- main.py
+++ main.py
@@ -15,10 +15,59 @@ except ImportError:
 
 global Sentencer, Ranker
 
-sGen = Sentencer()
-ranker = Ranker()
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print "Websocket connected"
+        #Send each word of each sentance
+        for i in range(0, ranker.t_index):
+            sen = ranker.getData(i)
+            self.write_message(sen)
 
-for i in range(0, 10):
-    sen = sGen.getSentenceString()
-    ranker.addToTable(sen)
-ranker.printAll()
+
+        #for w in words:
+            #self.write_message(w);
+
+
+    def on_message(self, message):
+        #self.write_message(u"Your message was: " + message)
+        results = message
+        print "Results from client: " + message
+
+    def on_close(self):
+        print "Websocket destroyed"
+
+
+class IndexPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', IndexPageHandler),
+            (r'/websocket', WebSocketHandler)
+        ]
+
+        settings = {
+            'template_path': 'templates'
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+if __name__ == '__main__':
+    #Generate sentances and init table
+    sGen = Sentencer()
+    ranker = Ranker()
+
+    for i in range(0, 10):
+        sen = sGen.getSentenceString()
+        ranker.addToTable(sen)
+    ranker.printAll()
+
+    print "Starting server..."
+    ws_app = Application()
+    server = tornado.httpserver.HTTPServer(ws_app)
+    server.listen(3000)
+    print "Listening on port 3000"
+    print ""
+    tornado.ioloop.IOLoop.instance().start()
